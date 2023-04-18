fde07f705d73f8e5593cf41fe9a33eb6527e8d60
mohamed9974 <e204472@metu.edu.tr>
2023-04-18 13:14:00 +0300

    push my customizations

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -14,4 +14,9 @@ return {
     }
     return config -- return final config table
   end,
+  require("null-ls").setup({
+    on_init = function(new_client, _)
+      new_client.offset_encoding = 'utf-32'
+    end,
+  })
 }
