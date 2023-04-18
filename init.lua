b4fd980f04154b636640e611414caa04971ab210
mohamed9974 <mohamed.amin@metu.edu.tr>
2023-04-18 14:46:37 +0300

    added remote

--- init.lua
+++ init.lua
@@ -11,13 +11,13 @@ return {
     show_changelog = true, -- show the changelog after performing an update
     auto_quit = false,     -- automatically quit the current session after a successful update
     remotes = {            -- easily add new remotes to track
-      --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
+        ["remote_name"] = "https://github.com/mohamed9974/nvim_config", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
     },
   },
   -- Set colorscheme to use
-  colorscheme = "tokyonight",
+  colorscheme = "tokyonight-night",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
