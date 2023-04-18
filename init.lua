742b53300d7c31e2e2003a94527a8a86c9fff1f4
mohamed9974 <e204472@metu.edu.tr>
2023-04-18 15:26:02 +0300

    made it tokyonight night

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "cyberpunk",
+  colorscheme = "tokyonight-night",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
