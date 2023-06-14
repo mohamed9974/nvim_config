2037ab55df9b9c3cb973dea7affb4e0ecdb54e7f
mohamed9974 <mohamed.amin@metu.edu.tr>
2023-06-14 22:31:27 +0300

    added last mods

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ return {
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
     },
   },
-
+  -- this is bs 
   -- Set colorscheme to use
   colorscheme = "oxocarbon",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
