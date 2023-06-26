5070ab5bd1cc6138b721a9faafe6e43af0ee1414
mohamed9974 <mohamed.amin@metu.edu.tr>
2023-06-26 11:11:41 +0300

    updated plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,13 @@
 return {
   -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
+  {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    opts = {}
+ }
+
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.colorscheme.tokyonight-nvim" }
 }
